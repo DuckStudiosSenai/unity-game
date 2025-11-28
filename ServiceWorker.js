b37f1fb43a9355671faacb54dd3614afd7fcb7e5
GabrielSelvenca <gabriel.selvenca@icloud.com>
2025-11-28 16:55:41 -0300

    New Unity build with chunked data and site communication

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DuckStudios-DuckStudiosReviewGame-5.0";
+const cacheName = "DuckStudios-DuckStudiosReviewGame-6.0";
 const contentToCache = [
     "Build/Build.loader.js",
     "Build/Build.framework.js",
